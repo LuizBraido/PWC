6bb647816ff5daae391906996d15952af7068bd8
Luiz Braido <120274031+LuizBraido@users.noreply.github.com>
2022-12-30 09:37:48 -0300

    Update

--- pwc.java
+++ pwc.java
@@ -7,7 +7,7 @@ public class pwc{
         System.out.print("Informe o endereco: ");
         String frase = sc.nextLine();
         System.out.print("Rua: ");
-        do{
+        while(cont < frase.length() && teste == true){
             char caracter = frase.charAt(cont);
             if((caracter == 32) || (caracter >= 65) && (caracter <= 90) || (caracter >= 97) && (caracter <= 122)){
                 System.out.print(frase.charAt(cont));
@@ -15,7 +15,7 @@ public class pwc{
             } else {
                 teste = false;
             }
-        } while(cont < frase.length() && teste == true);
+        }
         System.out.print("\nNumero: ");
         System.out.print(frase.substring(cont, frase.length()));
         sc.close();
