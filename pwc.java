d8e22bb56b9f9a36a2607615375c3d7dfcf2702b
Luiz Braido <120274031+LuizBraido@users.noreply.github.com>
2022-12-30 09:04:37 -0300

    Backup

---
+++ pwc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class pwc{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cont = 0;
+        boolean teste = true;
+        String rua = "";
+        System.out.print("Informe o endereco: ");
+        String frase = sc.nextLine();
+        System.out.print("Rua: ");
+        do{
+            char caracter = frase.charAt(cont);
+            if((caracter == 32) || (caracter >= 65) && (caracter <= 90) || (caracter >= 97) && (caracter <= 122)){
+                System.out.print(frase.charAt(cont));
+                //rua = rua + caracter;
+                cont++;
+            } else {
+                teste = false;
+            }
+        } while(cont < frase.length() && teste == true);
+        System.out.print("\nNumero: ");
+        System.out.print(frase.substring(cont, frase.length()));
+        sc.close();
+    }
+}
